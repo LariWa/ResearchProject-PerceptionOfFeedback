71cb29413b7a4454c109578160cfa30f493ff662
Lari <larissa.wagnerberger@gmail.com>
2021-09-28 13:56:47 +0200

    next/previous page

--- hello-world/src/components/Mainpage.vue
+++ hello-world/src/components/Mainpage.vue
@@ -1,25 +1,106 @@
 <template>
   <div class="hello">
     <h1>{{ "Title" }}</h1>
-    <question v-bind:question="question1" />
+    <div v-for="(contentItem, index) in content" :key="contentItem">
+      <div v-show="index == currentPage">
+        Page {{ index }}
+        <Page v-bind:pageContent="contentItem" />
+      </div>
+    </div>
+    <button v-on:click="previous" v-show="isPrevious()">Previous</button>
+
+    <button v-on:click="next" v-show="isNext()">
+      Next
+    </button>
   </div>
 </template>
 
 <script>
-import question from "./question.vue";
+import Page from "./page.vue";
 
 export default {
   name: "Mainpage",
   components: {
-    question,
+    Page,
   },
+  methods: {
+    next() {
+      this.currentPage++;
+    },
+    previous() {
+      this.currentPage--;
+    },
+    isNext() {
+      return this.currentPage < this.content.length - 1;
+    },
+    isPrevious() {
+      return this.currentPage > 0;
+    },
+  },
+
   data: function() {
     return {
-      question1: {
-        text: "Question 1",
-        responses: [{ text: "Wrong" }, { text: "Right!", correct: true }],
-        feedback: "visualaudio", //visual, audio, visualaudio or none
-      },
+      currentPage: 0,
+      content: [
+        [
+          {
+            text: "first text",
+            question: {
+              text: "Question 1",
+              responses: [{ text: "Wrong" }, { text: "Right!", correct: true }],
+              feedback: "visualaudio", //visual, audio, visualaudio or none
+            },
+          },
+          {
+            text: "second text",
+            question: {
+              text: "Question 2",
+              responses: [{ text: "Wrong" }, { text: "Right!", correct: true }],
+              feedback: "audio", //visual, audio, visualaudio or none
+            },
+          },
+        ],
+
+        //second page
+        [
+          {
+            text: "second page first text",
+            question: {
+              text: "Question 1",
+              responses: [{ text: "Wrong" }, { text: "Right!", correct: true }],
+              feedback: "visualaudio", //visual, audio, visualaudio or none
+            },
+          },
+          {
+            text: "second text",
+            question: {
+              text: "Question 2",
+              responses: [{ text: "Wrong" }, { text: "Right!", correct: true }],
+              feedback: "audio", //visual, audio, visualaudio or none
+            },
+          },
+        ],
+
+        //third page
+        [
+          {
+            text: "third page first text",
+            question: {
+              text: "Question 1",
+              responses: [{ text: "Wrong" }, { text: "Right!", correct: true }],
+              feedback: "visualaudio", //visual, audio, visualaudio or none
+            },
+          },
+          {
+            text: "second text",
+            question: {
+              text: "Question 2",
+              responses: [{ text: "Wrong" }, { text: "Right!", correct: true }],
+              feedback: "audio", //visual, audio, visualaudio or none
+            },
+          },
+        ],
+      ],
     };
   },
 };
